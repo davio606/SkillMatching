97b515e56fe14151da2c6a83a97d0f8d032e1007
Zijia Zeng <zz3tq@virginia.edu>
2019-02-17 21:05:13 -0500

    Set Up Heroku

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for myapp project.
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
